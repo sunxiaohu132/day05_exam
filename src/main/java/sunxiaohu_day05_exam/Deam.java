2b05b8be13de33d2c0d12dce4103b449981f87b3
Your Name <you@example.com>
2020-01-03 08:41:48 +0800

    xiao

--- src/main/java/sunxiaohu_day05_exam/Deam.java
+++ src/main/java/sunxiaohu_day05_exam/Deam.java
@@ -2,6 +2,6 @@ package sunxiaohu_day05_exam;
 
 public class Deam {
 	public static void main(String[] args) {
-		System.out.println("Hello Word!");
+		System.out.println("Hello Word! I am sunxiaohu");
 	}
 }
